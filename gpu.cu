e7e0c5212c854e199b8d2183d2a04ccecad9f09f
Surya Gullapalli <suryakiran.gullapalli@gmail.com>
2019-03-24 16:51:34 +0530

    minor modification

--- gpu.cu
+++ gpu.cu
@@ -25,19 +25,18 @@ thrustTest()
     // mark even indices with a 1; odd indices with a 0
     int                        values[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     thrust::device_vector<int> d_values(values, values + 10);
+
     // scatter all even indices into the first half of the
     // range, and odd indices vice versa
-    // int                        map[10] = {0, 5, 1, 6, 2, 7, 3, 8, 4, 9};
-    int                        map[10] = {0, 1, 5, 6, 8, 3, 4, 2, 7, 9};
+    int                        map[10] = {0, 5, 1, 6, 2, 7, 3, 8, 4, 9};
     thrust::device_vector<int> d_map(map, map + 10);
     thrust::device_vector<int> d_output(10);
     thrust::scatter(thrust::device, d_values.begin(), d_values.end(), d_map.begin(), d_output.begin());
-    thrust::host_vector<int> out(d_output);
 
+    thrust::host_vector<int> out(d_output);
     for (auto item : out) {
         std::cout << item << '\t';
     }
     std::cout << std::endl;
-    // d_output is now {1, 1, 1, 1, 1, 0, 0, 0, 0, 0}
     std::cout << " ***** Thrust Test end *****" << std::endl;
 }
